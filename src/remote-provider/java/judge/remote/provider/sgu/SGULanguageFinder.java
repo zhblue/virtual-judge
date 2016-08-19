7eb26deadfaed90c71aaf348f1a5cc67de904c6c
S.H. Lee <hnshhslsh@users.noreply.github.com>
2016-08-20 02:27:44 +0800

    update sgu  gcc/g++/c++14 submission info

--- src/remote-provider/java/judge/remote/provider/sgu/SGULanguageFinder.java
+++ src/remote-provider/java/judge/remote/provider/sgu/SGULanguageFinder.java
@@ -30,8 +30,9 @@ public class SGULanguageFinder implements LanguageFinder {
     @Override
     public LinkedHashMap<String, String> getDefaultLanguages() {
         LinkedHashMap<String, String> languageList = new LinkedHashMap<String, String>();
-        languageList.put("GNU C (MinGW, GCC 4)", "GNU C (MinGW, GCC 4)");
-        languageList.put("GNU CPP (MinGW, GCC 4)", "GNU CPP (MinGW, GCC 4)");
+        languageList.put("GNU C (MinGW, GCC 5)", "GNU C (MinGW, GCC 5)");
+        languageList.put("GNU CPP (MinGW, GCC 5)", "GNU CPP (MinGW, GCC 5)");
+        languageList.put("GNU CPP 14 (MinGW, GCC 5)", "GNU CPP 14 (MinGW, GCC 5)");
         languageList.put("Visual Studio C++ 2010", "Visual Studio C++ 2010");
         languageList.put("C#", "C#");
         languageList.put("Visual Studio C 2010", "Visual Studio C 2010");

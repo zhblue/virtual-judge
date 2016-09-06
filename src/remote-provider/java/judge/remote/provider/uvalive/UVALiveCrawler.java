869d64ae1cbb028cc9ef0039ee05465ab04d8c8e
hnshhslsh <lishihao200@gmail.com>
2016-09-07 00:15:25 +0800

    UVALive :download the PDF of the problem to VJ server and show it in problem page

--- src/remote-provider/java/judge/remote/provider/uvalive/UVALiveCrawler.java
+++ src/remote-provider/java/judge/remote/provider/uvalive/UVALiveCrawler.java
@@ -8,6 +8,7 @@ import judge.httpclient.SimpleHttpResponse;
 import judge.remote.RemoteOjInfo;
 import judge.remote.crawler.RawProblemInfo;
 import judge.remote.crawler.SyncCrawler;
+import judge.remote.shared.FileDownloader;
 import judge.tool.HtmlHandleUtil;
 import judge.tool.Tools;
 import org.apache.commons.lang3.StringUtils;
@@ -74,14 +75,26 @@ public class UVALiveCrawler extends SyncCrawler {
         info.memoryLimit = 0;
         info.source = Tools.regFind(htmlOuter, "<br />\\s*(.+)<br />\\s*<h3>" + problemId1).trim();
         info.url = outerUrl;
-
+        
+        String pdfURL = "https://icpcarchive.ecs.baylor.edu/external/" + Integer.parseInt(problemId1) / 100 + "/" + problemId1 + ".pdf";
+        
+        try{
+            String path = this.getClass().getResource("/").getPath().
+                    replace("WEB-INF/classes/", "problem/UVALivePDF/" +  Integer.parseInt(problemId1) / 100);
+            FileDownloader.downLoadFromUrl(pdfURL,path);
+            pdfURL =  "UVALivePDF/" +  Integer.parseInt(problemId1) / 100 + "/" + problemId1 + ".pdf";
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        
+        
         String descriptionPrefix =
                 "<style type=\"text/css\">h1,h2,h3,h4,h5,h6{margin-bottom:0;}div.textBG p{margin: 0 0 0.0001pt;}</style>" +
-                        "<span style='float:right'><a target='_blank' href='https://icpcarchive.ecs.baylor.edu/external/" + Integer.parseInt(problemId1) / 100 + "/" + problemId1 + ".pdf'><img width='100' height='26' border='0' title='Download as PDF' alt='Download as PDF' src='https://icpcarchive.ecs.baylor.edu/components/com_onlinejudge/images/button_pdf.png'></a></span><div style='clear:both'></div>";
+                        "<span style='float:right'><a target='_blank' href='" + pdfURL + "'><img width='100' height='26' border='0' title='Download as PDF' alt='Download as PDF' src='https://icpcarchive.ecs.baylor.edu/components/com_onlinejudge/images/button_pdf.png'></a></span><div style='clear:both'></div>";
         String sampleInput = Tools.regFind(htmlInner, "Sample Int?put</A>&nbsp;</FONT>\\s*</H2>([\\s\\S]*?)<H2><FONT size=\"4\" COLOR=\"#ff0000\"><A NAME=\"SECTION000100\\d000000000000000\"");
         String sampleOutput = Tools.regFind(htmlInner, "Sample Output</A>&nbsp;</FONT>\\s*</H2>([\\s\\S]*)");
         if (StringUtils.isEmpty(sampleInput) || StringUtils.isEmpty(sampleOutput)) {
-            info.description = descriptionPrefix + htmlInner;
+            info.description = descriptionPrefix + htmlInner +  "<p><embed width=\"100%\" height=\"700\" src=\"" + pdfURL + "\"> </embed></p> ";;
         } else {
             info.description = descriptionPrefix + Tools.regFind(htmlInner, "^([\\s\\S]*?)<H2><FONT size=\"4\" COLOR=\"#ff0000\"><A NAME=\"SECTION000100\\d000000000000000\"");
             info.input = Tools.regFind(htmlInner, "Int?put</A>&nbsp;</FONT></H2>([\\s\\S]*?)<H2><FONT size=\"4\" COLOR=\"#ff0000\"><A NAME=\"SECTION000100\\d000000000000000\"");

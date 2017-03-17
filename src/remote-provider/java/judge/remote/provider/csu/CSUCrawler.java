4bfbcaa113e01479ebc57d7c4299d841a40f9a64
hnshhslsh <lishihao200@gmail.com>
2017-03-17 20:38:47 +0800

    Update CSUCrawler.java

--- src/remote-provider/java/judge/remote/provider/csu/CSUCrawler.java
+++ src/remote-provider/java/judge/remote/provider/csu/CSUCrawler.java
@@ -28,9 +28,9 @@ public class CSUCrawler extends SimpleCrawler {
 
     @Override
     protected void populateProblemInfo(RawProblemInfo info, String problemId, String html) {
-        info.title = Tools.regFind(html, "<h1>\\d+:([\\s\\S]*?)</h1>").trim();
-        info.timeLimit = (1000 * Integer.parseInt(Tools.regFind(html, "Time Limit: </span><span.*?>(\\d+) Sec")));
-        info.memoryLimit = (1024 * Integer.parseInt(Tools.regFind(html, "Memory Limit: </span><span.*?>(\\d+) MB")));
+        info.title = Tools.regFind(html, "<h1>\\s*<a.*?>\\s*\\d+:([\\s\\S]*?)\\s*</a></h1>").trim();
+        info.timeLimit = (1000 * Integer.parseInt(Tools.regFind(html, "Time Limit:\\s*<span.*?>(\\d+) Sec")));
+        info.memoryLimit = (1024 * Integer.parseInt(Tools.regFind(html, "Memory Limit:\\s*<span.*?>(\\d+) MB")));
         info.description = (Tools.regFind(html, "<h2.*?>Description</h2>([\\s\\S]*?)\\s*</div>\\s*<div name=\"Input"));
         info.input = (Tools.regFind(html, "<h2.*?>Input</h2>([\\s\\S]*?)\\s*</div>\\s*<div name=\"Output"));
         info.output = (Tools.regFind(html, "<h2.*?>Output</h2>([\\s\\S]*?)\\s*</div>\\s*<div name=\"Sample Input"));
